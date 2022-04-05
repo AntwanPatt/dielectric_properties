506f09c97e213aacfec418c5a28ae4ef3e487e9f
Antoine Patt <patt.antoine@gmail.com>
2022-04-05 12:40:24 +0200

    Started to adjust the code for exception handling

--- prop_dielec.py
+++ prop_dielec.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python
 
-""" This script makes use of the total dipole moment
-    statistical distribution to compute the dielectric
-    constant of the system.
+""" This script makes use of the total dipole moment (M) statistical
+    distribution to compute the dielectric constant of the system.
+    The distribution of M is obtained by molecular dynamics simulations. Only
+    LAMMPS at the moment.
 """
 import numpy as np
 import numpy.linalg as nl
 
 ## some constants
-eps0 = 8.85418781762039e-12 # vacuum permittivity
-kb   = 1.380649e-23         # boltzmann constant
-e2C  = 1.602176634e-19      # elementary charge in C
-D2eA = 0.20819434           # debye to e.A
-fac  = 1.112650021e-59      # conversion factor Debye^2->C^2m^2
+eps_0 = 8.85418781762039e-12 # vacuum permittivity
+kB    = 1.380649e-23         # boltzmann constant
+e2C   = 1.602176634e-19      # elementary charge in C
+D2eA  = 0.20819434           # debye to e.A
+fac   = 1.112650021e-59      # conversion factor Debye^2->C^2m^2
 
 class prop_dielec:
     """Pyton class to compute dielectric properties"""
 
     def __init__(self, tot_dip_moments, arg2):
-        self.M = tot_dip_moments
+        # Getting total dipole moments distribution and converting it to Debye
+        self.M = tot_dip_moments / D2eA
+
         self.truc = arg2
 
     def static_eps(self):
@@ -32,19 +35,26 @@ class prop_dielec:
             M2_avg[i] = np.mean( nl.norm(Mtemp, axis = 1) ** 2 )
             Mavg_2[i] = nl.norm( np.mean(Mtemp, axis = 0) ) ** 2
 
-            ## calculating variance and M ratio for convergence
-            Mdiff  = M2_avg - Mavg_2
-            Mratio = np.divide(Mavg_2, M2_avg)
+        ## calculating variance and M ratio for convergence
+        Mdiff  = M2_avg - Mavg_2
+        Mratio = np.divide(Mavg_2, M2_avg)
 
-            ## calculating static dielectric constant (relative permittivity)
-            temperature = np.mean(np.loadtxt("avg.res", skiprows=2, usecols=(1)))
-            volume      = np.mean(np.loadtxt("avg.res", skiprows=2, usecols=(4)))
+        ## need temperature and volume to calculate
+        eps_r = 0.0
+        try:
+            temperature = np.mean(np.loadtxt("avg2.res", skiprows=2, usecols=(1)))
+            volume      = np.mean(np.loadtxt("avg2.res", skiprows=2, usecols=(4)))
+        except FileNotFoundError:
+            print("Pas de fichier avg2 au fait")
+        else:
+            ## calculating static dielectric constant (relative permittivity), eps_r
+            eps_r = 1 + (Mdiff * fac) / (3 * eps_0 * volume * 1e-30 * kB * temperature)
 
-            eps = 1 + (Mdiff * fac) / (3 * eps0 * volume * 1e-30 * kb * temperature )
+        return eps_r
 
 ################################################################################
 
 # test code of the above class
 if __name__ == '__main__':
-    A = np.ones(3)
+    A = np.ones((3,3))
     test = prop_dielec(A, "haha")
